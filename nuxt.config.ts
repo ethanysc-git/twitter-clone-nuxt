32c0da300c69e35fc0d640c6066e44ab5f0adf5b
Ethan Chen <acuratnam@gmail.com>
2024-06-10 23:37:07 +0800

    Server added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+
+  runtimeConfig: {
+      jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+      jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+
+      cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+      cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+      cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+  }
 })
